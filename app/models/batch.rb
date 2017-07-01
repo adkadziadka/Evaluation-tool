e3202b85427e57c428775797d0a17ed8f803e75c
adkadziadka <industrialdog@gmail.com>
2017-07-01 21:17:19 +0200

    new algorithm option

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -18,16 +18,49 @@ class Batch < ApplicationRecord
 		}
 	end
 
+#algorithm
+
 	def get_student
-		students_array = []
-		students.each do |student|
-			student.performances.each do |performance|
-				students_array += [student] if performance.rate == 3
-				students_array += [student, student] if performance.rate == 2
-				students_array += [student, student, student] if performance.rate == 1
-			end
-		end
-		students_array.sample
+		students = []		
+		loop do
+			rate = [1, 2, 3].sample
+			students = Student.joins(:performances).where(batch_id: id, 'performances.rate' => rate)
+			break if !students.empty?
+		end		
+		students.sample
 	end
+
+	
+
+	# another:
+
+	# def get_student
+	# 	students_array = []
+	# 	loop do
+	# 		rate = [1, 2, 3].sample
+	# 		students.each do |student|
+	# 			student.performances.each do |performance|
+	# 				students_array << student.performances == rate
+	# 			end
+	# 		end
+	# 		break if !students_array.empty?
+	# 	end
+	# 	students_array.sample
+	# end
+
+
+	# best choice:
+
+	# def get_student
+	# 	students_array = []
+	# 	students.each do |student|
+	# 		student.performances.each do |performance|
+	# 			students_array += [student] if performance.rate == 3
+	# 			students_array += [student, student] if performance.rate == 2
+	# 			students_array += [student, student, student] if performance.rate == 1
+	# 		end
+	# 	end
+	# 	students_array.sample
+	# end
 end
 	
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Batch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:batch) {Batch.new(number: 1, start_date: DateTime.parse("2017-05-11"), end_date: DateTime.parse("2017-07-11"))}
+
+	let(:student) {Student.new(picture: "https://goo.gl/r5cw83", full_name: "John Doe", batch: batch1)}
+
+	let(:performance) {Performance.new(student: student1, rate: 1, remarks: "Lorem imsum dolor sin amet", date: Date.today)}
+  describe 'get_student' do
+  	it 'gets a student' do
+  		batch.get_student.class == Student
+  	end
+  end
 end
