e28363d07724b290cf4d6d37b4e49a0776834183
adkadziadka <industrialdog@gmail.com>
2017-07-04 15:19:14 +0200

    algorithm change

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -20,15 +20,15 @@ class Batch < ApplicationRecord
 
 #algorithm
 
-	def get_student
-		students = []		
-		loop do
-			rate = [1, 2, 3].sample
-			students = Student.joins(:performances).where(batch_id: id, 'performances.rate' => rate)
-			break if !students.empty?
-		end		
-		students.sample
-	end
+	# def get_student
+	# 	students = []		
+	# 	loop do
+	# 		rate = [1, 2, 3].sample
+	# 		students = Student.joins(:performances).where(batch_id: id, 'performances.rate' => rate)
+	# 		break if !students.empty?
+	# 	end		
+	# 	students.sample
+	# end
 
 	
 
@@ -52,17 +52,17 @@ class Batch < ApplicationRecord
 
 	# # best choice:
 
-	# def get_student
-	# 	students_array = []
-	# 	students.each do |student|
-	# 		student.performances.each do |performance|
-	# 			students_array += [student] if performance.rate == 3
-	# 			students_array += [student, student] if performance.rate == 2
-	# 			students_array += [student, student, student] if performance.rate == 1
-	# 		end
-	# 	end
-	# 	students_array.sample
-	# end
+	def get_student
+		students_array = []
+		students.each do |student|
+			student.performances.each do |performance|
+				students_array += [student] if performance.rate == 3
+				students_array += [student, student] if performance.rate == 2
+				students_array += [student, student, student] if performance.rate == 1
+			end
+		end
+		students_array.sample
+	end
 
 	def self.search(search)
 		if search
